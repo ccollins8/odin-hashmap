9cf9b7b96c977fcab7b015b26b7ddfa2ca2dfb1c
ccollins8 <48657302+ccollins8@users.noreply.github.com>
2024-04-28 19:31:12 -0700

    Update set, has, and hash
    hash now used % to get smaller values. set and has
    now takes into account linked list.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import "./style.css";
 
+class Node {
+    constructor(key = null, value = null) {
+        this.key = key
+        this.value = value
+        this.nextNode = null
+    }
+}
+
 class HashMap {
 
     constructor() {
@@ -13,39 +21,67 @@ class HashMap {
       
         const primeNumber = 31;
         for (let i = 0; i < key.length; i++) {
-            hashCode = primeNumber * hashCode + key.charCodeAt(i);
+            hashCode = primeNumber * hashCode + key.charCodeAt(i)
         }
 
-        return hashCode; 
+        return hashCode % this.table.length; 
     }
 
     set(key, value) {
+        const newNode = new Node(key, value)
+
         const index = this.hash(key)
-        if (this.has(key) == false) {
-            this.capacity++
+        if (!this.table[index]) {
+            this.table[index] = newNode
+        } else {
+            let current = this.table[index]
+            while (current.nextNode != null) {
+                current = current.nextNode
+            }
+            current.nextNode = new Node(key,value)
         }
-        this.table[index] = [key, value]
+        // const index = this.hash(key)
+        // if (this.has(key) == false) {
+        //     this.capacity++
+        // }
+        // this.table[index] = [key, value]
     }
 
     get(key) {
-        const index = this.hash(key)
-        if (this.has(key)) {
-            return this.table[index][1]
-        } else {
-            return null
-        }
+        // const index = this.hash(key)
+        // if (this.has(key)) {
+        //     return this.table[index][1]
+        // } else {
+        //     return null
+        // }
     }
 
     has(key) {
         const index = this.hash(key)
-        if (this.table[index]) {
-            if (this.table[index][0] == key) {
-                return true
-            } else {
-                return false
-            }
+
+        if (!this.table[index]) {
+            return false
         }
+
+        let current = this.table[index]
+        while (current) {
+            if (current.key == key) {
+                return true
+            }
+            current = current.nextNode
+        }
+
         return false
+
+        // const index = this.hash(key)
+        // if (this.table[index]) {
+        //     if (this.table[index][0] == key) {
+        //         return true
+        //     } else {
+        //         return false
+        //     }
+        // }
+        // return false
     }
 
     remove(key) {
@@ -63,6 +99,21 @@ class HashMap {
         return this.capacity
     }
 
+    clear() {
+        this.capacity = 0
+        this.table = new Array()
+    }
+
+    keys() {
+        const arr = []
+        for (let i = 0; i < this.table.length; i++) {
+            if (this.table[i]) {
+                arr.push(this.table[i][0])
+            }
+        }
+        return arr
+    }
+
 }
 
 const m = new HashMap()
@@ -70,10 +121,8 @@ const m = new HashMap()
 m.set('name', 'corey')
 m.set('email', 'abc@gmail.com')
 m.set('phone', '123-456-7890')
+m.set('jon', 'Smith')
 
-console.log(m.get('phone'))
-console.log(m.has('name'))
-console.log(m.length())
-console.log(m.remove('name'))
-console.log(m.length())
+console.log(m.hash('Jon'))
+
 console.log(m.table)
